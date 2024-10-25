f02b807c0b1a63f614bc88ce50daed9fa22f25d6
Haridas Pawar <haridaspawar0203@gmail.com>
2024-10-25 17:00:34 +0530

    Compress images for hero section

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>ARCHTYPE</title>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= LINK; ?>assets/vendor/bootstrap/css/bootstrap.min.css" />
     <link rel="stylesheet" href="<?= LINK; ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" />
     <link rel="stylesheet" href="<?= LINK; ?>assets/vendor/owlcarousel/assets/owl.carousel.css" />
